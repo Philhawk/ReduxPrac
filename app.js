d08d6614712081c4d51cb70e00718aaf1fa31081
Philhawk <pkjacob@gmail.com>
2016-10-18 10:03:55 -0400

    damn commas

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-'use strict'
 
 const express = require('express');
 const bookRoutes = require('./routes/books');
@@ -6,22 +5,22 @@ const errorHandlingRoutes = require('./routes/errors');
 const chapterRoutes = require('./routes/chapters');
 const numberOfVisitsRoutes = require('./routes/numVisits');
 const bodyParser = require('body-parser');
-const session = require('express-session')
+const session = require('express-session');
 
 const app = express();
 
 app.use(session({
   secret: 'mango pancakes',
   resave: false,
-  saveUninitialized: true
-}))
+  saveUninitialized: true,
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', errorHandlingRoutes);
 
-app.use('/files', express.static(__dirname + '/public/static'));
+app.use('/files', express.static(`${__dirname}/public/static`));
 
 app.use('/api/numVisits', numberOfVisitsRoutes);
 app.use('/api/books', bookRoutes);
@@ -29,8 +28,8 @@ app.use('/api/books', chapterRoutes);
 
 
 app.use((err, req, res, next) => {
-  err.length ? res.sendStatus(403) : res.sendStatus(500)
-})
+  err.message === 'potato' ? res.sendStatus(403) : res.sendStatus(500);
+});
 
 
 module.exports = app;
--- routes/chapters.js
+++ routes/chapters.js
@@ -1,7 +1,5 @@
-'use strict'
-
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 const Book = require('../models/book.js')
 const Chapter = require('../models/chapter.js')
@@ -21,26 +19,26 @@ router.get('/:bookId/chapters', (req, res, next) => {
 router.post('/:bookId/chapters', (req, res, next) => {
   Book.findById(req.params.bookId)
     .then(book => {
-      return book.createChapter(req.body)
+      return book.createChapter(req.body);
     })
     .then(chapter => res.status(201).send(chapter))
     .catch(next)
-})
+});
 
 router.get('/:bookId/chapters/:chapterId', (req, res, next) => {
   Chapter.findById(req.params.chapterId)
     .then(chapter => {
       if (chapter) {
         if (chapter.bookId === +req.params.bookId) {
-          res.send(chapter)
+          res.send(chapter);
         } else {
           res.status(404).send()
         }
       } else {
-        res.status(404).send()
+        res.status(404).send();
       }
     })
-    .catch(next)
+    .catch(next);
 })
 
 router.put('/:bookId/chapters/:chapterId', (req, res, next) => {
@@ -49,15 +47,15 @@ router.put('/:bookId/chapters/:chapterId', (req, res, next) => {
       if (chapter) {
         if (chapter.bookId === +req.params.bookId) {
           chapter.updateAttributes(req.body)
-            .then(chapter => res.send(chapter))
+            .then(newlyUpdatedChapter => res.send(newlyUpdatedChapter))
         } else {
-          res.status(404).send()
+          res.status(404).send();
         }
       } else {
         res.status(404).send()
       }
     })
-    .catch(next)
+    .catch(next);
 })
 
 router.delete('/:bookId/chapters/:chapterId', (req, res, next) => {
@@ -66,12 +64,12 @@ router.delete('/:bookId/chapters/:chapterId', (req, res, next) => {
       if (chapter) {
         if (chapter.bookId === +req.params.bookId) {
           chapter.destroy()
-            .then(() => res.status(204).send())
+            .then(() => res.status(204).send());
         } else {
           res.status(404).send()
         }
       } else {
-        res.status(404).send()
+        res.status(404).send();
       }
     })
     .catch(next)
